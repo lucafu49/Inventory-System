01af678c381c8e1e6630a232d2ad92caa7be3a95
lucafu49 <lucasmena1403@gmail.com>
2024-09-10 12:29:12 -0300

    Agregar archivos de proyecto.

---
+++ SafeInventory/Forms/Form1.cs
@@ -0,0 +1,30 @@
+﻿using SafeInventory.Models;
+using SafeInventory.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SafeInventory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ProductServices ps = new ProductServices();
+            List<Product> products = new List<Product>();
+            products = ps.GetProducts();
+            foreach (Product p in products)
+            {
+                MessageBox.Show(p.Name);
+            }
+        }
+    }
+}
---
+++ SafeInventory/Services/ProductServices.cs
@@ -0,0 +1,22 @@
+﻿using SafeInventory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeInventory.Services
+{
+    public class ProductServices
+    {
+        private SafeInventoryEntities db;
+        
+        public ProductServices() {
+            db = new SafeInventoryEntities();
+        }
+
+        public List<Product> GetProducts() { 
+            return db.Product.ToList();
+        }
+    }
+}
